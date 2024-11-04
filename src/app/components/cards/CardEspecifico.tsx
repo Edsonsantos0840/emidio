2bf0e185df8f450f8ad3aa30af60f27b061e00fa
Edson Santos <129888422+Edsonsantos0840@users.noreply.github.com>
2024-11-04 16:55:14 -0300

    ajustes card específico.

--- src/app/components/cards/CardEspecifico.tsx
+++ src/app/components/cards/CardEspecifico.tsx
@@ -31,7 +31,7 @@ export default function CardEspecifico(props: CardEspecificoProps) {
       </div>
       <article className=" pt-2 hidden group-hover:alinha6 gap-2 m-auto">
         <h2 className="titulo respoFont">{props.titulo}</h2>
-        <ul className="text-md">
+        <ul className="text-md md:text-2xl">
           <li className="font-terceiraFont list-disc text-md">
             {props.artigo1}
           </li>
@@ -65,7 +65,7 @@ export default function CardEspecifico(props: CardEspecificoProps) {
         </ul>
         <Link
           href={"/contato"}
-          className="md:w-full m-auto bg-[var(--corLetra2)] p-2 md:p-3 text-center text-[var(--corFundo)] rounded-md shadow-md hover:scale-105 hover:bg-[var(--terceira-cor)] w-[300px] text-[1.2rem] respoFont ease-in duration-300"
+          className="md:w-full m-auto bg-[var(--corLetra2)] p-2 md:p-3 text-center text-[var(--corFundo)] rounded-md shadow-md hover:scale-105 hover:bg-[var(--terceira-cor)] w-[300px] text-[1.2rem] md:text-[2rem] respoFont ease-in duration-300"
         >
           {props.agenda}
         </Link>
