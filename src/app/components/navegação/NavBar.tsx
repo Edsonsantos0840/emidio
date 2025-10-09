0945b715248b3019a6d59a885c095d3abcca89a4
Edson Santos <edsonpsantos0840@gmail.com>
2025-10-09 01:40:33 +0000

    correçao no navBar

--- src/app/components/navegação/NavBar.tsx
+++ src/app/components/navegação/NavBar.tsx
@@ -12,15 +12,15 @@ function NavBar() {
   return (
     <header className="fixed w-full z-30 bg-fundo shadow-lg">
       <nav
-        className="container mx-auto flex justify-between items-center h-[70px] px-4"
+        className="container mx-auto flex justify-between items-center px-2"
         role="navigation"
         aria-label="Menu principal"
       >
         {/* Logo e nome */}
         <div className="flex items-center gap-3 w-full">
-          <figure className="relative w-[80px] h-[80px] bg-cover">
+          <figure className="relative  bg-cover">
             <Image
-              src="/image/2.png"
+              src="/2.png"
               alt="Logo do Espaço Emídio & Sá"
               fill
               sizes="(max-width: 768px) 100px, (max-width: 1024px) 50vw, 33vw"
@@ -33,11 +33,11 @@ function NavBar() {
             aria-label="Marca do site"
             className="flex justify-start items-baseline gap-2 w-full"
           >
-            <h1 className="text-4xl text-corLetra md:text-2xl lg:text-3xl font-bold">
+            <h1 className="text-4xl text-corLetra md:text-2xl  font-bold">
               Espaço
             </h1>
             <div>
-              <h2 className="text-3xl md:text-xl lg:text-2xl text-corLetra lg:text-letra2">
+              <h2 className="text-3xl md:text-xl  text-corLetra lg:text-letra2">
                 Emídio & Sá
               </h2>
               <p className="text-xs text-corLetra lg:text-principal font-terceiraFont">
@@ -63,9 +63,9 @@ function NavBar() {
           id="nav-menu"
           className={`${
             isOpen ? "block" : "hidden"
-          } absolute top-[70px] left-0 w-full bg-corSombra lg:bg-transparent lg:static lg:flex lg:items-center lg:justify-end`}
+          } absolute  left-0 w-full bg-corSombra lg:bg-transparent lg:static lg:flex lg:items-center lg:justify-end`}
         >
-          <ul className="flex flex-col lg:flex-row gap-2 lg:gap-8 text-center text-fundo lg:text-corLetra font-fontPrincipal p-4 lg:p-0">
+          <ul className="flex flex-col lg:flex-row gap-2 text-center text-fundo lg:text-corLetra font-fontPrincipal ">
             {NavFields.map((link) => (
               <li key={link.text}>
                 <Link href={link.href} className="linkNav block py-1">
